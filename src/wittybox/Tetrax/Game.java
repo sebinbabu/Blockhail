d7c7ea27b3d5b578eb9ca459beb470450b4699c2
Sebin Babu <sebin1998@gmail.com>
2018-09-20 12:22:37 +0530

    fully integrated checks

--- src/wittybox/Tetrax/Game.java
+++ src/wittybox/Tetrax/Game.java
@@ -4,41 +4,174 @@ import wittybox.Tetrax.*;
 import java.io.IOException;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 
 public class Game {
 	private Shape activeShape = null;
-	private int activeShapeX = 0;
-	private int activeShapeY = 0;
+	private Point activeShapePos = null;
 	private Board board = null;
 	private DefaultTerminalFactory terminalFactory = null;
 	private Terminal terminal = null;
 
-	public void updateBoard() {
-		this.activeShapeX = 5;
-		this.activeShapeY = 5;
+	public boolean rotateShape() {
+		this.activeShape.rotateRight();
+		Point []points = this.activeShape.getPoints();
+		int potentialX = this.activeShapePos.getX(), potentialY = this.activeShapePos.getY();
+		int x, y;
+
+		for(Point pos : points) {
+			x = pos.getX() + potentialX;
+			y = pos.getY() + potentialY;
+			if(y < 0) {
+				if(!this.moveShapeRight()) {
+					this.activeShape.rotateLeft();
+					return false;
+				} else {
+					return true;
+				}
+			}
+			if(y >= this.board.getCols()) {
+				if(!this.moveShapeLeft()) {
+					this.activeShape.rotateLeft();
+					return false;
+				} else {
+					return true;
+				}
+			}
+			if(x >= this.board.getRows()) {
+				this.activeShape.rotateLeft();
+				return false;
+			}
+
+			if(this.board.get(x, y)) {
+				this.activeShape.rotateLeft();
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void setNewShape() {
+		this.activeShape = Shapes.getRandomShape();
+		this.activeShapePos = new Point(3, 6);
+	}
+
+	public void pasteShape() {
 		Point []points = this.activeShape.getPoints();
 		for(Point p : points) {
-			this.board.set(p.getX() + activeShapeX, p.getY() + activeShapeY, true);
+			this.board.set(p.getX() + this.activeShapePos.getX(), p.getY() + this.activeShapePos.getY(), true);
+		}
+	}
+
+	public boolean moveShapeLeft() {
+		int potentialX = this.activeShapePos.getX();
+		int potentialY = this.activeShapePos.getY() - 1;
+		Point []points = this.activeShape.getPoints();
+		int res;
+
+		for(Point pos : points) {
+			res = pos.getY() + potentialY;
+			if(res < 0 || res >= this.board.getCols())
+				return false;
+			if(this.board.get(potentialX + pos.getX(), res))
+				return false;
+		}
+		this.activeShapePos.setY(potentialY);
+		return true;
+	}
+
+	public boolean moveShapeRight() {
+		int potentialX = this.activeShapePos.getX();
+		int potentialY = this.activeShapePos.getY() + 1;
+		Point []points = this.activeShape.getPoints();
+		int res;
+
+		for(Point pos : points) {
+			res = pos.getY() + potentialY;
+			if(res < 0 || res >= this.board.getCols())
+				return false;
+			if(this.board.get(potentialX + pos.getX(), res))
+				return false;
+		}
+		this.activeShapePos.setY(potentialY);
+		return true;
+	}
+
+	public boolean moveShapeDown() throws IOException{
+		int potentialX = this.activeShapePos.getX() + 1;
+		int potentialY = this.activeShapePos.getY();
+		Point []points = this.activeShape.getPoints();
+		int res;
+		for(Point pos : points) {
+			res = pos.getX() + potentialX;
+			if(res >= this.board.getRows()) {
+				this.pasteShape();
+				this.setNewShape();
+				return false;
+			}
+			if(this.board.get(res, potentialY + pos.getY())) {
+				this.pasteShape();
+				this.setNewShape();
+				return false;
+			}
+		}
+		this.activeShapePos.setX(potentialX);
+		return true;
+	}
+
+	public KeyType getBlockingInput() throws IOException {
+		return this.terminal.readInput().getKeyType();
+	}
+
+	public KeyType getNonBlockingInput() throws IOException {
+		return this.terminal.pollInput().getKeyType();
+	}
+
+	public void clearShape() {
+		Point []points = this.activeShape.getPoints();
+		for(Point p : points) {
+			this.board.set(p.getX() + this.activeShapePos.getX(), p.getY() + this.activeShapePos.getY(), false);			
 		}
 	}
 
 	public void displayBoard() throws IOException {
-		board.display();
+		this.pasteShape();
+		this.board.display();
+		this.clearShape();
 	}
 
 	public Game() throws IOException {
 		this.terminalFactory = new DefaultTerminalFactory();
 		this.terminal = terminalFactory.createTerminal();
+		this.terminal.enterPrivateMode();
+        this.terminal.setCursorVisible(false);
+		
 		this.activeShape = Shapes.getRandomShape();
+		this.activeShapePos = new Point(3, 6);
+
 		this.board = new Board(terminal); 
 	}
 
 	public static void main(String args[]) {
 		try {
 			Game game = new Game();
-			game.updateBoard();
-			game.displayBoard();
-			Thread.sleep(3000);
+			KeyType key;
+
+			while(true) {
+				game.displayBoard();
+				key = game.getBlockingInput();
+				if(key == KeyType.ArrowUp) {
+					game.rotateShape();
+				} else if(key == KeyType.ArrowLeft) {
+					game.moveShapeLeft();
+				} else if(key == KeyType.ArrowRight) {
+					game.moveShapeRight();
+				} else if(key == KeyType.ArrowDown) {
+					game.moveShapeDown();
+				}
+
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
